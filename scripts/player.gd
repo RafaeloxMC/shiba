a6e1f23a63c94bcc66d8c4bc09f5a963d934545d
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-09-11 19:18:33 +0200

    Add jump sfx & light acceleration to movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,13 @@ extends CharacterBody2D
 
 
 const SPEED = 130.0
+const ACCELERATION = 1000.0
 const JUMP_VELOCITY = -300.0
 var DEAD = false
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func die():
 	DEAD = true
@@ -28,22 +30,20 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 		return
 		
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		audio_stream_player_2d.play()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		animated_sprite_2d.animation = "walk"
-		velocity.x = direction * SPEED
+		animated_sprite_2d.play("walk")
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta)
 		if velocity.x > 0:
 			animated_sprite_2d.flip_h = false;
 		else:
 			animated_sprite_2d.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animated_sprite_2d.animation = "idle"
+		animated_sprite_2d.play("idle")
 
 	move_and_slide()

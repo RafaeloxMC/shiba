e278ffb3475b41c4c1e60d83a28632ffcbda693e
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-09-12 08:12:33 +0200

    Fix animation for dog food

--- scripts/dog_food.gd
+++ scripts/dog_food.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	if GameManager.is_food_blacklisted(self):
@@ -10,3 +11,4 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.name != "Player": return
 	GameManager.eat_food(self, body as CharacterBody2D)
 	animation_player.play("pickup")
+	animated_sprite_2d.play("pickup")

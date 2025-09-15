1b3af45f51b71b49b61692efe723b7d78a4f752d
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-09-15 20:41:31 +0200

    Fix dialog triggered by other objects than player

--- scripts/dialog_trigger.gd
+++ scripts/dialog_trigger.gd
@@ -8,7 +8,9 @@ func _ready() -> void:
 	if GameManager.is_dialog_blacklisted(self):
 		self.queue_free()
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
+	if body.name != "Player":
+		return
 	GameManager.call_dialog(text, author, sprite_frames)
 	GameManager.dialog_blacklist.push_back(self.transform)
 	self.queue_free()

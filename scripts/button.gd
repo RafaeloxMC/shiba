2e3a036ee1273e33a3bf8ed66c77a12f2f6af58f
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-09-28 08:48:52 +0200

    Add volume sliders

--- scripts/button.gd
+++ scripts/button.gd
@@ -5,6 +5,7 @@ extends Area2D
 @export var scene: PackedScene
 @export var type: int
 @export var settings: Array
+var original_text: String
 var original_scale: Vector2
 var hover_scale: Vector2 = Vector2(1.05, 1.05)
 var tween_duration: float = 0.2
@@ -12,6 +13,7 @@ var selected = false
 var current_value = 0
 
 func _ready():
+	original_text = label.text
 	original_scale = animated_sprite_2d.scale
 	if type == 1:
 		animated_sprite_2d.play("settings")
@@ -37,5 +39,31 @@ func next() -> void:
 	else:
 		current_value += 1
 	label.text = settings[current_value]
-	GameManager.difficulty = current_value
-	print("Set difficulty to " + label.text)
+	if original_text.to_lower() == "difficulty":
+		GameManager.difficulty = current_value
+		print("Set difficulty to " + label.text)
+	else:
+		
+		if original_text.to_lower() == "volume":
+			var bus = AudioServer.get_bus_index("Master")
+			var volume = -40 + (4.0 * int(label.text) / 10)
+			volume = clamp(volume, -80, 0)
+			AudioServer.set_bus_volume_db(bus, volume)
+			print(str(AudioServer.get_bus_volume_db(bus)))
+
+		if original_text.to_lower() == "music volume":
+			var bus = AudioServer.get_bus_index("Music")
+			var volume = -40 + (4.0 * int(label.text) / 10)
+			volume = clamp(volume, -80, 0)
+			AudioServer.set_bus_volume_db(bus, volume)
+			print(str(AudioServer.get_bus_volume_db(bus)))
+
+		if original_text.to_lower() == "sfx volume":
+			var bus = AudioServer.get_bus_index("SFX")
+			var volume = -40 + (4.0 * int(label.text) / 10)
+			volume = clamp(volume, -80, 0)
+			AudioServer.set_bus_volume_db(bus, volume)
+			print(str(AudioServer.get_bus_volume_db(bus)))
+
+
+	
--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,18 +1,35 @@
 extends Control
 
-@onready var difficulty: Area2D = $difficulty
-@onready var credits: Area2D = $credits
-@onready var back: Area2D = $back
+@onready var difficulty: Area2D = $btn_difficulty
+@onready var credits: Area2D = $btn_credits
+@onready var back: Area2D = $btn_back
 
 @onready var title: Label = $title
 @onready var subtitle: Label = $subtitle
 @onready var hint: Label = $hint
 
-var current_button_id = 1
+@onready var arrow_down: AnimatedSprite2D = $arrow_down
+@onready var arrow_up: AnimatedSprite2D = $arrow_up
+
+var current_button_id = 0
 var credits_open = false
+var first_visible_button = 0
+
+var buttons: Array[Area2D]
+var button_positions: Array[Vector2]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	buttons.clear()
+	for child in self.get_children():
+		if child.name.begins_with("btn_"):
+			buttons.push_back(child)
+	button_positions = [
+		buttons[0].position,
+		buttons[1].position if buttons.size() > 1 else buttons[0].position,
+		buttons[2].position if buttons.size() > 2 else buttons[0].position
+	]
+	update_visible_buttons()
 	get_current_button().selected = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,60 +38,72 @@ func _process(_delta: float) -> void:
 		title.hide()
 		subtitle.hide()
 		hint.hide()
-		difficulty.hide()
-		credits.hide()
-		back.hide()
+		for button in buttons:
+			button.hide()
 		return
 	else:
 		credits_open = false
 		title.visible = true
 		subtitle.visible = true
 		hint.visible = true
-		difficulty.visible = true
-		credits.visible = true
-		back.visible = true
+		update_visible_buttons()
 	if Input.is_action_just_pressed("ui_up"):
 		last()
 	elif Input.is_action_just_pressed("ui_down"):
 		next()
 	elif Input.is_action_just_pressed("ui_accept"):
 		submit()
+
+func update_visible_buttons() -> void:
+	for i in range(buttons.size()):
+		if i >= first_visible_button and i < first_visible_button + 3:
+			buttons[i].visible = true
+			buttons[i].position = button_positions[i - first_visible_button]
+		else:
+			buttons[i].visible = false
+	
+	if first_visible_button == 0:
+		arrow_up.visible = false
+		arrow_down.visible = true
+	elif first_visible_button + 3 >= buttons.size():
+		arrow_up.visible = true
+		arrow_down.visible = false
+	else:
+		arrow_up.visible = true
+		arrow_down.visible = true
 		
 
 func next():
 	get_current_button().selected = false
-	if current_button_id >= 3:
-		current_button_id = 1
-	else:
-		current_button_id += 1
+	current_button_id = (current_button_id + 1) % buttons.size()
+	if current_button_id < first_visible_button or current_button_id >= first_visible_button + 3:
+		first_visible_button = current_button_id - 2 if current_button_id >= 2 else 0
+	update_visible_buttons()
 	get_current_button().selected = true
-		
+
 func last():
 	get_current_button().selected = false
-	if current_button_id <= 1:
-		current_button_id = 3
-	else:
-		current_button_id -= 1
+	current_button_id = (current_button_id - 1 + buttons.size()) % buttons.size()
+	if current_button_id < first_visible_button:
+		first_visible_button = current_button_id
+	elif current_button_id >= first_visible_button + 3:
+		first_visible_button = current_button_id - 2
+	update_visible_buttons()
 	get_current_button().selected = true
 
 func submit():
-	if get_current_button() == credits:
-		var node = load(get_current_button().scene.resource_path)
+	var current_button = get_current_button()
+	if current_button == credits:
+		var node = load(current_button.scene.resource_path)
 		add_child(node.instantiate())
 		credits_open = true
-	elif get_current_button().scene != null:
-		if get_current_button() != back:
-			get_tree().change_scene_to_packed(get_current_button().scene)
-	elif get_current_button().type == 1:
-		get_current_button().next()
-	elif get_current_button() == back:
+	elif current_button.scene != null:
+		if current_button != back:
+			get_tree().change_scene_to_packed(current_button.scene)
+	elif current_button.type == 1:
+		current_button.next()
+	elif current_button == back:
 		queue_free()
-	
+
 func get_current_button() -> Area2D:
-	if current_button_id == 1:
-		return difficulty
-	elif current_button_id == 2:
-		return credits
-	elif current_button_id == 3:
-		return back
-	return null
+	return buttons[current_button_id]

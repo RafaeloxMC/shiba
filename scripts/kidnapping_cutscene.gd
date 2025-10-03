9f099e5fcb795db1169eefce26b4d6a414ee4219
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-10-03 08:33:02 +0200

    Separate dog house coins & add skip cutscene

--- scripts/kidnapping_cutscene.gd
+++ scripts/kidnapping_cutscene.gd
@@ -4,9 +4,16 @@ const robo_shiba: SpriteFrames = preload("res://assets/sprite_frames/robo_shiba.
 const shibina: SpriteFrames = preload("res://assets/sprite_frames/shibina.tres")
 
 @onready var animation_player: AnimationPlayer = $Camera2D/Control/CanvasLayer/ColorRect/AnimationPlayer
+@onready var cutscene_anim: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	animation_player.play("RESET")
+	cutscene_anim.speed_scale = 1
+	
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("jump"):
+		cutscene_anim.speed_scale = 8
+		print("Skipped!")
 
 func call_robo_dialog():
 	GameManager.call_dialog("You will come with me now! You better not resist or you will regret it!", "Robo Shiba", robo_shiba)

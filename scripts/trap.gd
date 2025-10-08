da784780bfd4abb67beae99873f2f87122348e5d
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-10-08 15:58:06 +0200

    Add has_ramp_l & _r properties to trap

--- scripts/trap.gd
+++ scripts/trap.gd
@@ -1,13 +1,21 @@
 extends Area2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var collision_shape_2d: CollisionShape2D = $Killzone/CollisionShape2D
+@onready var collision_shape_2d: CollisionShape2D = $Killzone/KillzoneCollisionShape
+@onready var ramp_l: CollisionPolygon2D = $StaticBody2D/RampL
+@onready var ramp_r: CollisionPolygon2D = $StaticBody2D/RampR
 
 @export var upside_down: bool = false
+@export var has_ramp_l: bool = true
+@export var has_ramp_r: bool = true
 
 func _ready() -> void:
 	if upside_down:
 		self.rotation_degrees = 180
+	if not has_ramp_l:
+		ramp_l.queue_free()
+	if not has_ramp_r:
+		ramp_r.queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":

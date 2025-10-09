db17bda67b8ca6613cba70d7ca48d49fc9db658c
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-10-09 12:37:35 +0200

    Add difficulty property to trap

--- scripts/trap.gd
+++ scripts/trap.gd
@@ -9,6 +9,8 @@ extends Area2D
 @export var has_ramp_l: bool = true
 @export var has_ramp_r: bool = true
 
+var delay = 1
+
 func _ready() -> void:
 	if upside_down:
 		self.rotation_degrees = 180
@@ -16,10 +18,19 @@ func _ready() -> void:
 		ramp_l.queue_free()
 	if not has_ramp_r:
 		ramp_r.queue_free()
+		
+	if GameManager.difficulty == 3:
+		delay = 0.25
+	if GameManager.difficulty == 2:
+		delay = 0.5
+	if GameManager.difficulty == 1:
+		delay = 0.75
+	if GameManager.difficulty == 0:
+		delay = 1
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		await get_tree().create_timer(1).timeout
+		await get_tree().create_timer(delay).timeout
 		var anim = "attack"
 		animated_sprite_2d.play(anim)
 		collision_shape_2d.disabled = false

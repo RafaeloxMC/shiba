2e058dc96cb49efff41846820c56ebf57d97beea
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-10-18 09:58:23 +0200

    Improve cracked ice & fix water in all levels

--- scripts/cracked_ice.gd
+++ scripts/cracked_ice.gd
@@ -13,8 +13,8 @@ func _ready():
 func _on_body_exited(body: Node2D) -> void:
 	if not body.name == "Player":
 		return
-	await get_tree().create_timer(timeout).timeout
-	animated_sprite_2d.play_backwards("break")
+	await get_tree().create_timer(timeout + 2).timeout
+	animated_sprite_2d.play_backwards("crack")
 	await get_tree().create_timer(0.75).timeout
 	animated_sprite_2d.play("idle")
 	collision_shape_2d.set_deferred("disabled", false)
@@ -24,7 +24,9 @@ func _on_body_entered(body: Node2D) -> void:
 	if not body.name == "Player":
 		return
 	await get_tree().create_timer(timeout).timeout
-	animated_sprite_2d.play("break")
-	await get_tree().create_timer(0.75).timeout
+	animated_sprite_2d.play("crack")
+	await get_tree().create_timer(0.4).timeout
 	collision_shape_2d.set_deferred("disabled", true)
+	animated_sprite_2d.play("break")
+	await get_tree().create_timer(0.4).timeout
 	animated_sprite_2d.set_deferred("visible", false)

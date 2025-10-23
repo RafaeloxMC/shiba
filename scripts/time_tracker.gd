c7c2508eb53a6fb6a8388750c1b6fbffcca9df3a
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-10-23 17:34:14 +0200

    Fix jittery time tracker

--- scripts/time_tracker.gd
+++ scripts/time_tracker.gd
@@ -1,9 +1,12 @@
 extends Label
 
+func _ready():
+	horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+	custom_minimum_size.x = 80
+
 func _process(delta: float) -> void:
 	GameManager.time += delta
-	@warning_ignore("narrowing_conversion")
-	var mins: int = GameManager.time / 60
+	var mins: int = int(GameManager.time / 60)
 	var seconds: int = int(GameManager.time) % 60
 	var milliseconds: int = int((GameManager.time - int(GameManager.time)) * 100)
-	self.text = str(mins).pad_zeros(2) + " : " + str(seconds).pad_zeros(2) + " : " + str(milliseconds).pad_zeros(2)
+	text = "%02d : %02d : %02d" % [mins, seconds, milliseconds]

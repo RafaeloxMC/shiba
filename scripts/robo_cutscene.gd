50b89f80b01a8fbf2a7e2d2e40d63570cac1aaf3
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-10-24 18:10:50 +0200

    Finish implementation of Robo Cutscene

--- scripts/robo_cutscene.gd
+++ scripts/robo_cutscene.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 var robo_sprite_frames = preload("res://assets/sprite_frames/robo_shiba.tres")
+var shiba_sprite_frames = preload("res://assets/sprite_frames/player.tres")
 
 func call_first_robo_dialog():
 	GameManager.call_dialog("I've been watching you. What an honor to see you here...", "Robo Dog", robo_sprite_frames)
+
+func call_second_robo_dialog():
+	GameManager.call_dialog("Oh, you wish I would. Get her back yourself!", "Robo Dog", robo_sprite_frames)
+
+func call_first_shiba_dialog():
+	GameManager.call_dialog("Not you again... Let Shibina go!", "Shiba", shiba_sprite_frames)
